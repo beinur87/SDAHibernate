b18500206bdb33ceca3e9513d09b732613de1129
beinur87 <beinur.nurla@gmail.com>
2023-05-13 10:16:14 +0300

    exercitiul 1

--- src/main/java/org/example/exercitiu2/Ex2.java
+++ src/main/java/org/example/exercitiu2/Ex2.java
@@ -1,2 +1,30 @@
-package org.example.exercitiu2;public class Ex2 {
+package org.example.exercitiu2;
+
+import org.hibernate.*;
+import org.hibernate.cfg.Configuration;
+
+
+public class Ex2 {
+    public static void main(String[] args) {
+        SessionFactory sessionFactory = new Configuration()
+                .configure("hibernate.config.xml")
+                .addAnnotatedClass(Student.class)
+                .buildSessionFactory();
+        Session session = sessionFactory.openSession();
+        Transaction transaction = session.beginTransaction();
+
+        Student stud1 = new Student(1, "Maria",1987);
+        Student stud2 = new Student(2, "George",1996);
+        Student stud3 = new Student(3, "Nicu",1967);
+
+        session.persist(stud1);
+        session.persist(stud2);
+        session.persist(stud3);
+
+
+
+        transaction.commit();
+        session.close();
+
+    }
 }
--- src/main/java/org/example/exercitiu2/Student.java
+++ src/main/java/org/example/exercitiu2/Student.java
@@ -1,2 +1,51 @@
-package org.example.exercitiu2;public class Student {
+package org.example.exercitiu2;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+@Table(name = "students")
+@Entity
+public class Student {
+    @Id
+    private Integer id;
+    @Column(nullable = false)
+    private String name;
+    @Column(name = "birth_year",nullable = false)
+    private Integer birthYear;
+
+public Student(){
+
+}
+
+    public Student(Integer id, String name, Integer birthYear) {
+        this.id = id;
+        this.name = name;
+        this.birthYear = birthYear;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Integer getBirthYear() {
+        return birthYear;
+    }
+
+    public void setBirthYear(Integer birthYear) {
+        this.birthYear = birthYear;
+    }
 }
